c35de0616b9c51a5af78d6daf9444149e34d6103
azaza12345 <61620351+azaza12345@users.noreply.github.com>
2022-01-06 09:43:57 +0300

    add routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,16 @@ import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
 import {FormsModule} from "@angular/forms";
 import {MessagesComponent} from "./messages/messages.component";
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeroesComponent,
-    MessagesComponent
+    MessagesComponent,
+    DashboardComponent,
+    HeroDetailComponent
   ],
     imports: [
         BrowserModule,
